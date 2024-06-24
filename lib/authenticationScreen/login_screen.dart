4fb69e472a8e4001d6eacb3b79a163b2368a9b3c
Alejandro Zepeda <zepedaalex25@gmail.com>
2024-06-24 14:16:42 -0700

    adding initial files

---
+++ lib/authenticationScreen/login_screen.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 120,
+              ),
+              Image.asset("images/logo.png",
+                width: 300,
+              ),
+              Text(
+                "Welcome!",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                "Login to find your next match.",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
