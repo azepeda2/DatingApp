4f37196196fda5621ba0048251b86e7cdb9395a7
Alejandro Zepeda <zepedaalex25@gmail.com>
2024-07-04 01:03:11 -0700

    finished login screen

--- lib/authenticationScreen/login_screen.dart
+++ lib/authenticationScreen/login_screen.dart
@@ -1,3 +1,7 @@
+import "dart:ui";
+
+import "package:datingapp/widgets/custom_text_field_widget.dart";
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 
 class LoginScreen extends StatefulWidget {
@@ -8,6 +12,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  bool showProgressBar = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +42,94 @@ class _LoginScreenState extends State<LoginScreen> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              const SizedBox(
+                height: 28,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 30,
+                height: 50,
+                child: CustomTextFieldWidget(
+                  editingController: emailTextEditingController,
+                  labelText: "Email",
+                  iconData: Icons.email_outlined,
+                  isObscure: false,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 30,
+                height: 50,
+                child: CustomTextFieldWidget(
+                  editingController: passwordTextEditingController,
+                  labelText: "Password",
+                  iconData: Icons.lock_outline,
+                  isObscure: true,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width / 3,
+                height: 50,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                  ),
+                ),
+                child: InkWell(
+                  onTap: () {
+
+                  },
+                  child: const Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account? ",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+
+                    },
+                    child: const Text(
+                      "Register here",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                  ),
+                ]
+              ),
+
+              showProgressBar == true
+                  ? const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.pink),
+              )
+                  : Container()
             ],
           ),
         ),
